079f0368ccdfd3d64304fdc23799fd248977430d
pumashanp <94768721+pumashanp@users.noreply.github.com>
2022-11-06 16:44:53 -0500

    Add files via upload

---
+++ Untitled1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+import pandas as np
+
+a = [90,88,97,95,78,81,84,82,83,82,45,46,32,14,25,29,31]
+b = [99,70,95,90,78,81,84,82,83,82,45,46,32,14,25,29,31]
+numpy.correlate (v1, v2, mode,old_behaviour)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
